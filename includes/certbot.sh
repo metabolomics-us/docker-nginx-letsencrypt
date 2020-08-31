c0ce99b7808846c01b33f39386d52756da9209e1
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2020-08-31 11:56:49 -0400

    added support for multiple domain certificates

--- includes/certbot.sh
+++ includes/certbot.sh
@@ -1,10 +1,13 @@
 if [[ ! -f /var/www/certbot ]]; then
     mkdir -p /var/www/certbot
 fi
-certbot certonly \
+
+# Iterate over provided comma-separated domains
+for x in $(echo $DOMAIN | sed "s/,/ /g"); do
+    certbot certonly \
         --config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
 		--agree-tos \
-		--domains "$DOMAIN" \
+		--domains "$x" \
 		--email "$EMAIL" \
 		--expand \
 		--noninteractive \
@@ -12,7 +15,12 @@ certbot certonly \
 		--webroot-path /var/www/certbot \
 		$OPTIONS || true
 
-if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" ]]; then
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" /usr/share/nginx/certificates/privkey.pem
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
-fi
+    if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$x/privkey.pem" ]]; then
+        cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$x/privkey.pem" /usr/share/nginx/certificates/$x/privkey.pem
+        cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$x/fullchain.pem" /usr/share/nginx/certificates/$x/fullchain.pem
+
+        # Set default certificate for single domain applications
+        cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$x/privkey.pem" /usr/share/nginx/certificates/privkey.pem
+        cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$x/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
+    fi
+done
--- includes/entrypoint.sh
+++ includes/entrypoint.sh
@@ -2,18 +2,24 @@
 # Create a self signed default certificate, so Ngix can start before we have
 # any real certificates.
 
-# Ensure we have folders available
+# Iterate over provided comma-separated domains
+for x in $(echo $DOMAIN | sed "s/,/ /g"); do
+    # Ensure we have folders available
+    if [[ ! -f /usr/share/nginx/certificates/$x/fullchain.pem ]]; then
+        mkdir -p /usr/share/nginx/certificates/$x
+    fi
 
-if [[ ! -f /usr/share/nginx/certificates/fullchain.pem ]]; then
-    mkdir -p /usr/share/nginx/certificates
-fi
+    if [[ ! -f /usr/share/nginx/certificates/$x/cert.crt ]]; then
+        openssl genrsa -out /usr/share/nginx/certificates/$x/privkey.pem 4096
+        openssl req -new -key /usr/share/nginx/certificates/$x/privkey.pem -out /usr/share/nginx/certificates/$x/cert.csr -nodes -subj \
+            "/C=PT/ST=World/L=World/O=${x:-ilhicas.com}/OU=Ilhicas/CN=${x:-ilhicas.com}/EMAIL=${EMAIL:-info@ilhicas.com}"
+        openssl x509 -req -days 365 -in /usr/share/nginx/certificates/$x/cert.csr -signkey /usr/share/nginx/certificates/$x/privkey.pem -out /usr/share/nginx/certificates/$x/fullchain.pem
+    fi
 
-if [[ ! -f /usr/share/nginx/certificates/cert.crt ]]; then
-    openssl genrsa -out /usr/share/nginx/certificates/privkey.pem 4096
-    openssl req -new -key /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/cert.csr -nodes -subj \
-    "/C=PT/ST=World/L=World/O=${DOMAIN:-ilhicas.com}/OU=Ilhicas/CN=${DOMAIN:-ilhicas.com}/EMAIL=${EMAIL:-info@ilhicas.com}"
-    openssl x509 -req -days 365 -in /usr/share/nginx/certificates/cert.csr -signkey /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/fullchain.pem
-fi
+    # Set default certificate for single domain applications
+    cp /usr/share/nginx/certificates/$x/privkey.pem /usr/share/nginx/certificates/privkey.pem
+    cp /usr/share/nginx/certificates/$x/fullchain.pem /usr/share/nginx/certificates/fullchain.pem
+done
 
 # Send certbot Emission/Renewal to background
 $(while :; do /opt/certbot.sh; sleep "${RENEW_INTERVAL:-12h}"; done;) &
